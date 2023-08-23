b250884b08ccd592b7eddecb4bdd92422cf37c64
shahedalazzam <shahed.a.alazzam@gmail.com>
2023-08-23 13:40:24 +0300

    edit and add total to checkout

--- src/screens/CheckoutScreen/CheckoutScreen.js
+++ src/screens/CheckoutScreen/CheckoutScreen.js
@@ -4,44 +4,66 @@ import {
    ExpiryDate,
    Cvv,
    SubmitButton,
+
 } from "frames-react-native";
 
-import React from "react";
-import { StyleSheet, View, TouchableHighlight } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { useCartsContext } from "../../hooks/useCartContext";
 
 export default function CheckoutScreen() {
+   const { cards, dispatch } = useCartsContext();
+   const [price, setPrice] = useState(0)
+   const cartProducts = [
+   ];
+
+
+   const totalPrice = cartProducts.reduce(
+      (total, product) => total + product.price,
+      0
+   );
+   useEffect(() => {
+      let TotalPrice = 0
+      cards.map((item) => {
+         TotalPrice += item?.price
+      })
+      setPrice(TotalPrice)
+   }, [cards])
+   
+   console.log("price", price)
    return (
       <View style={styles.container}>
-         <Frames
-            config={{
-               debug: true,
-               publicKey: "pk_test_4296fd52-efba-4a38-b6ce-cf0d93639d8a",
-            }}
-            cardTokenized={(e) => {
-               alert(e.token);
-            }}
-         >
-            <CardNumber
-               style={styles.cardNumber}
-               placeholderTextColor="#9898A0"
-               // showIcon={false} in case you don't want to see the card scheme logo
-            />
-
-            <View style={styles.dateAndCode}>
-               <ExpiryDate
-                  style={styles.expiryDate}
+         <Text style={styles.total}>Total : ${price.toFixed(2)}</Text>
+         <View style={styles.cont}>
+            <Frames
+               config={{
+                  debug: true,
+               }}
+               cardTokenized={(e) => {
+                  alert(e.token);
+               }}
+            >
+               <CardNumber
+                  style={styles.cardNumber}
                   placeholderTextColor="#9898A0"
                />
-               <Cvv style={styles.cvv} placeholderTextColor="#9898A0" />
-            </View>
 
-            <SubmitButton
-               title="Pay Now"
-               style={styles.button}
-               textStyle={styles.buttonText}
-               onPress={() => console.log("merchant action")}
-            />
-         </Frames>
+               <View style={styles.dateAndCode}>
+                  <ExpiryDate
+                     style={styles.expiryDate}
+                     placeholderTextColor="#9898A0"
+                  />
+                  <Cvv style={styles.cvv} placeholderTextColor="#9898A0" />
+               </View>
+
+               <SubmitButton
+                  title="Pay Now"
+                  style={styles.button}
+                  textStyle={styles.buttonText}
+                  onPress={() => console.log("merchant action")}
+               />
+            </Frames>
+         </View>
       </View>
    );
 }
@@ -49,12 +71,17 @@ export default function CheckoutScreen() {
 const styles = StyleSheet.create({
    container: {
       flex: 1,
-      backgroundColor: "#000000",
       alignItems: "center",
       justifyContent: "flex-start",
       paddingTop: 80,
       paddingLeft: 10,
       paddingRight: 10,
+   },
+   total:{
+      marginTop:150,
+      fontWeight:500,
+      fontSize:15,
+      marginBottom:40,   
    },
    dateAndCode: {
       marginTop: 15,
@@ -64,8 +91,7 @@ const styles = StyleSheet.create({
    cardNumber: {
       fontSize: 18,
       height: 50,
-      color: "#FEFFFF",
-      backgroundColor: "#1B1C1E",
+      backgroundColor: "#fff",
       borderColor: "#3A4452",
       borderRadius: 5,
       borderWidth: 0,
@@ -74,16 +100,14 @@ const styles = StyleSheet.create({
       fontSize: 18,
       height: 50,
       width: "48%",
-      color: "#FEFFFF",
-      backgroundColor: "#1B1C1E",
+      backgroundColor: "#fff",
       borderWidth: 0,
    },
    cvv: {
       fontSize: 18,
       height: 50,
       width: "48%",
-      color: "#FEFFFF",
-      backgroundColor: "#1B1C1E",
+      backgroundColor: "#fff",
       borderWidth: 0,
    },
    button: {
@@ -91,9 +115,10 @@ const styles = StyleSheet.create({
       borderRadius: 5,
       marginTop: 20,
       justifyContent: "center",
-      backgroundColor: "#4285F4",
+      backgroundColor: "#FF9900",
    },
    buttonText: {
+      fontWeight:600,
       color: "white",
       fontSize: 16,
    },
