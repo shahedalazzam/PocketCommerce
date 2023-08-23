805f1de4bbadd657f27358b333aa799461c1d5d7
Basel Qarqa <basel.m.qarqa@gmail.com>
2023-08-23 20:50:39 +0300

    Payment button Done

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -3,33 +3,37 @@ import { createContext, useReducer } from "react";
 export const CartContext = createContext();
 
 export const cartReducer = (state, action) => {
-   switch (action.type) {
-      case "SET_CARD":
-         return {
-            cards: action.payload,
-         };
-      case "ADD_CARD":
-         return {
-            cards: [action.payload, ...state.cards],
-         };
-      case "DELETE_CARD":
-         return {
-            cards: state.cards.filter((w) => w._id !== action.payload._id),
-         };
-      default:
-         return state;
-   }
+  switch (action.type) {
+    case "SET_CARD":
+      return {
+        cards: action.payload,
+      };
+    case "ADD_CARD":
+      return {
+        cards: [action.payload, ...state.cards],
+      };
+    case "DELETE_CARD":
+      return {
+        cards: state.cards.filter((w) => w._id !== action.payload._id),
+      };
+    case "DELETE_ALL_CARD":
+      return {
+        cards: [],
+      };
+    default:
+      return state;
+  }
 };
 
 export const CartContextProvider = ({ children }) => {
-   const [state, dispatch] = useReducer(cartReducer, {
-      cards: [],
-   });
-   console.log("CardsContext state", state);
+  const [state, dispatch] = useReducer(cartReducer, {
+    cards: [],
+  });
+  console.log("CardsContext state", state);
 
-   return (
-      <CartContext.Provider value={{ ...state, dispatch }}>
-         {children}
-      </CartContext.Provider>
-   );
+  return (
+    <CartContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
 };
--- src/screens/CheckoutScreen/CheckoutScreen.js
+++ src/screens/CheckoutScreen/CheckoutScreen.js
@@ -1,125 +1,138 @@
 import {
-   Frames,
-   CardNumber,
-   ExpiryDate,
-   Cvv,
-   SubmitButton,
-
+  Frames,
+  CardNumber,
+  ExpiryDate,
+  Cvv,
+  SubmitButton,
 } from "frames-react-native";
 
 import React, { useEffect, useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Alert } from "react-native";
 import { useCartsContext } from "../../hooks/useCartContext";
+import { useNavigation } from "@react-navigation/native";
 
 export default function CheckoutScreen() {
-   const { cards, dispatch } = useCartsContext();
-   const [price, setPrice] = useState(0)
-   const cartProducts = [
-   ];
+  const { cards, dispatch } = useCartsContext();
+  const [price, setPrice] = useState(0);
+  const cartProducts = [];
+  const { navigate } = useNavigation();
 
+  const totalPrice = cartProducts.reduce(
+    (total, product) => total + product.price,
+    0
+  );
+  useEffect(() => {
+    let TotalPrice = 0;
+    cards.map((item) => {
+      TotalPrice += item?.price;
+    });
+    setPrice(TotalPrice);
+  }, [cards]);
 
-   const totalPrice = cartProducts.reduce(
-      (total, product) => total + product.price,
-      0
-   );
-   useEffect(() => {
-      let TotalPrice = 0
-      cards.map((item) => {
-         TotalPrice += item?.price
-      })
-      setPrice(TotalPrice)
-   }, [cards])
-   
-   console.log("price", price)
-   return (
-      <View style={styles.container}>
-         <Text style={styles.total}>Total : ${price.toFixed(2)}</Text>
-         <View style={styles.cont}>
-            <Frames
-               config={{
-                  debug: true,
-               }}
-               cardTokenized={(e) => {
-                  alert(e.token);
-               }}
-            >
-               <CardNumber
-                  style={styles.cardNumber}
-                  placeholderTextColor="#9898A0"
-               />
+  console.log("price", price);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.total}>Total : ${price.toFixed(2)}</Text>
+      <View style={styles.cont}>
+        <Frames
+          config={{
+            debug: true,
+          }}
+          cardTokenized={(e) => {
+            alert(e.token);
+          }}
+        >
+          <CardNumber
+            style={styles.cardNumber}
+            placeholderTextColor="#9898A0"
+          />
 
-               <View style={styles.dateAndCode}>
-                  <ExpiryDate
-                     style={styles.expiryDate}
-                     placeholderTextColor="#9898A0"
-                  />
-                  <Cvv style={styles.cvv} placeholderTextColor="#9898A0" />
-               </View>
+          <View style={styles.dateAndCode}>
+            <ExpiryDate
+              style={styles.expiryDate}
+              placeholderTextColor="#9898A0"
+            />
+            <Cvv style={styles.cvv} placeholderTextColor="#9898A0" />
+          </View>
 
-               <SubmitButton
-                  title="Pay Now"
-                  style={styles.button}
-                  textStyle={styles.buttonText}
-                  onPress={() => console.log("merchant action")}
-               />
-            </Frames>
-         </View>
+          <SubmitButton
+            title="Pay Now"
+            style={styles.button}
+            textStyle={styles.buttonText}
+            onPress={() => {
+              Alert.alert(
+                "Your Payment Transaction was don successfully!",
+                "Thank you for choosing OpenShop",
+                [
+                  {
+                    text: "OK",
+                    onPress: () => {
+                      dispatch({ type: "DELETE_ALL_CARD" });
+                      navigate("HomeScreen");
+                    },
+                  },
+                ]
+              );
+            }}
+          />
+        </Frames>
       </View>
-   );
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "flex-start",
-      paddingTop: 80,
-      paddingLeft: 10,
-      paddingRight: 10,
-   },
-   total:{
-      marginTop:150,
-      fontWeight:500,
-      fontSize:15,
-      marginBottom:40,   
-   },
-   dateAndCode: {
-      marginTop: 15,
-      flexDirection: "row",
-      justifyContent: "space-between",
-   },
-   cardNumber: {
-      fontSize: 18,
-      height: 50,
-      backgroundColor: "#fff",
-      borderColor: "#3A4452",
-      borderRadius: 5,
-      borderWidth: 0,
-   },
-   expiryDate: {
-      fontSize: 18,
-      height: 50,
-      width: "48%",
-      backgroundColor: "#fff",
-      borderWidth: 0,
-   },
-   cvv: {
-      fontSize: 18,
-      height: 50,
-      width: "48%",
-      backgroundColor: "#fff",
-      borderWidth: 0,
-   },
-   button: {
-      height: 50,
-      borderRadius: 5,
-      marginTop: 20,
-      justifyContent: "center",
-      backgroundColor: "#FF9900",
-   },
-   buttonText: {
-      fontWeight:600,
-      color: "white",
-      fontSize: 16,
-   },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 80,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  total: {
+    marginTop: 150,
+    fontWeight: 500,
+    fontSize: 15,
+    marginBottom: 40,
+  },
+  dateAndCode: {
+    marginTop: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  cardNumber: {
+    fontSize: 18,
+    height: 50,
+    backgroundColor: "#fff",
+    borderColor: "#3A4452",
+    borderRadius: 5,
+    borderWidth: 0,
+  },
+  expiryDate: {
+    fontSize: 18,
+    height: 50,
+    width: "48%",
+    backgroundColor: "#fff",
+    borderWidth: 0,
+  },
+  cvv: {
+    fontSize: 18,
+    height: 50,
+    width: "48%",
+    backgroundColor: "#fff",
+    borderWidth: 0,
+  },
+  button: {
+    height: 50,
+    borderRadius: 5,
+    marginTop: 20,
+    justifyContent: "center",
+    backgroundColor: "#FF9900",
+  },
+  buttonText: {
+    fontWeight: 600,
+    color: "white",
+    fontSize: 16,
+  },
 });
